e4c9dbec438012b01423c88230ab28d36074474e
Steffen Laube <laube.steffengmx.de>
2003-02-07 14:44:00 +0000

    - Licenze Header eingefügt - Schleifen  optimiert

--- src/utilities.c
+++ src/utilities.c
@@ -1,3 +1,12 @@
+/*************************************************************
+*
+* This is a part of ebotula.
+* It is distributed under the GNU General Public License
+* See the file COPYING for details.
+*
+* (c)2003 Steffen Laube <realebula@gmx.de>
+*************************************************************/
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
@@ -24,14 +33,15 @@ void stopParser(int sig) {
 
 void trim(char* line) {
 	int alpha,i,j;
-
+	int line_len;
 
 	// remove leading space
 	// remove multispace
 	// remove newline
 	// remove carge return
 	alpha=j=0;
-	for (i=0;i<=strlen(line);i++) {
+	line_len=strlen(line);
+	for (i=0;i<=line_len;i++) {
 		if ((line[i]==' ') && (alpha==1)) {
 			line[j]=line[i];
 			alpha=0;
@@ -52,9 +62,11 @@ void trim(char* line) {
 // ############################################################################# 
 void clearspace(char *line) {
 	int i,j,noclr;
+	int line_len;
 
+	line_len=strlen(line);
 	j=noclr=0;
-	for (i=0;i<=strlen(line);i++) {
+	for (i=0;i<=line_len;i++) {
 		if ((line[i]!=' ') || (noclr==1)) {
 			if ((line[i]=='\"') && (noclr==0)) {
 				noclr=1;
