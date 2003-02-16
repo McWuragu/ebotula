f26a9f13d2c476799f5e1a309a59c11eea0c7596
Steffen Laube <laube.steffengmx.de>
2003-02-16 09:43:46 +0000

    - default werte nach config.h verschoben

--- src/parameter.h
+++ src/parameter.h
@@ -11,16 +11,6 @@
 #define PARAMETER_H
 
 
-#define	PARAMETER_CHAR	'-'
-#define COMMENT_CHAR	'#'
-#define MAX_READ_BUFFER_SIZE 	256
-
-#define KEY_SERVER			"servername"
-#define KEY_PORT			"port"
-#define KEY_BOTNAME			"botname"
-#define KEY_REALNAME		"realname"
-#define KEY_THREADLIMIT		"threadlimit"
-#define	KEY_DATABASEPATH	"databasepath"
 
 
 void cmd_line(int argc,const char *argv[]);
