f5891d1be5627a002cfd8fb6214599977f950ca6
Steffen Laube <laube.steffengmx.de>
2003-02-19 22:37:21 +0000

    - add  the time mark for remove dead logins and accounts

--- src/timing.c
+++ src/timing.c
@@ -16,21 +16,32 @@
 
 void *synchron(void *argv){
 	extern ConfType setup;
-	time_t timestamp;
+	time_t newTime;
+	time_t lastRemoveDeadLogins=0;
+	time_t lastRemoveDeadAccounts=0;
 
 	// set the thread cancelable
 	pthread_setcancelstate(PTHREAD_CANCEL_ENABLE,NULL);
 	pthread_setcanceltype(PTHREAD_CANCEL_ASYNCHRONOUS,NULL);
 
-	DEBUG("Synchronize Thread is started.");
+	DEBUG("Synchronize Thread is running");
 
 	while (1) {
         sleep(90);
-		time(&timestamp);
+		time(&newTime);
 
-		DEBUG("Send ping");
 		ping(setup.server);
-		
-    }
 
+		// remove dead logins
+		if ((newTime-lastRemoveDeadLogins)>=(setup.AutoLoggoff*86400)) {
+			DEBUG("Remove dead logins");
+			time(&lastRemoveDeadLogins);
+		}
+
+		// remove dead accounts
+		if ((newTime-lastRemoveDeadAccounts)>=(setup.AccountLiveTime*86400)) {
+			DEBUG("Remove dead accounts");
+			time(&lastRemoveDeadAccounts);
+		}
+    }
 }
