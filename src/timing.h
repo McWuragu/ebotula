3190fa9ac3cbc3d2e48b761ac1b423f96c389579
Steffen Laube <laube.steffengmx.de>
2003-02-24 18:29:35 +0000

    - add comment

--- src/timing.h
+++ src/timing.h
@@ -10,6 +10,13 @@
 #ifndef TIMING_H
 #define TIMING_H
 
+/**
+* This is  the function for the timing thread. She handle all task which need a timing
+* synchronization. This task are ping the server, remove dead accounts, automatic log off
+* and reset the ban mask after the time ban.
+* @param argv is a pointer for any parameters This is unused but necessary for  pthread_create()
+* @return a pointer of any return values. This is unused but necessary for  pthread_create()
+*/
 void *synchron(void *argv);
 
 #endif
