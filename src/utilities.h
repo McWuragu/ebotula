27b9158733d86846ebfd8288050757cdd173fe82
Steffen Laube <laube.steffengmx.de>
2003-02-24 18:26:51 +0000

    - modify the comments

--- src/utilities.h
+++ src/utilities.h
@@ -10,14 +10,15 @@
 #ifndef UTILITIES_H
 #define UTILITIES_H
 
-#include <syslog.h>
 
 
-// Macro for debugging msg
+
 #ifdef _DEBUG
-	#include <stdio.h>
+	#include <syslog.h>
+	/** Macro for debugging messages */
     #define DEBUG(str...) syslog(LOG_DEBUG,str);
 #else
+	/** Macro for debugging messages */
     #define DEBUG(str...)  
 #endif
 
@@ -25,16 +26,16 @@
 /**
 * This function trim a string. It remove leading, folling and 
 * multi spaces and  CR LF at the end.
-* @param line a pointer of a string for modify
+* @param line is a pointer of a string to modify
 */	  
 void trim(char *line);
 void StrToLower(char *str);							 
 void print_msg(const char *msg[]);
 
 /**
-* clearspace() remove all spaces in a string until from double quote include spaces.
+* This function remove all spaces in a string until from double quote include spaces.
 * This space in double quote untouched.
-* @param line a pointer  of a string for modify
+* @param line is a pointer  of a string to modify
 */ 
 void clearspace(char *line);
 
