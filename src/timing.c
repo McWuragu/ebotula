22e24616bbadf210fb6475b92b7bcb6f3226b4a1
Steffen Laube <laube.steffengmx.de>
2003-03-02 12:40:09 +0000

    - modify the ping of  the server - time the  loop with 1s - ping  the server all half ping timeout time

--- src/timing.c
+++ src/timing.c
@@ -26,6 +26,7 @@
 void *synchron(void *argv){
 	extern ConfType sSetup;
 	time_t newTime;
+	time_t lastPing=0;
 	time_t lastRemoveDeadLogins=0;
 	time_t lastRemoveDeadAccounts=0;
 
@@ -36,24 +37,28 @@ void *synchron(void *argv){
 	DEBUG("Synchronize Thread is running");
 
 	while (1) {
-        msleep((sSetup.iTimeout*1000)/2);
+        sleep(1);
 		
 		time(&newTime);
 
-		ping(sSetup.server);
+		if ((newTime-lastPing)>=(sSetup.iTimeout/2)) {
+			ping(sSetup.server);
+			lastPing=newTime;
+		}
+		
 
 		// remove dead logins
 		if ((newTime-lastRemoveDeadLogins)>=3600) {
 			DEBUG("Remove dead logins");
 			rmDeadLogins(newTime-sSetup.AutoLoggoff*86400);
-			time(&lastRemoveDeadLogins);
+			lastRemoveDeadLogins=newTime;
 		}
 
 		// remove dead accounts
 		if ((newTime-lastRemoveDeadAccounts)>=3600) {
 			DEBUG("Remove dead accounts");
 			rmDeadAccounts(newTime-sSetup.AccountLiveTime*86400);
-			time(&lastRemoveDeadAccounts);
+			lastRemoveDeadAccounts=newTime;
 		}
     }
 }
