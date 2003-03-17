c464e5e1756d703b15793422fe7af8791a8fe40d
Steffen Laube <laube.steffengmx.de>
2003-03-17 17:30:34 +0000

    - added  the syntax description for all language

--- src/irchelp.h
+++ src/irchelp.h
@@ -16,8 +16,125 @@
 
 
 #ifdef LANG_DE
-	#include "lang/irchelp_ger.h"
+	#include "lang/irchelp_de.h"
 #else 
 	#include "lang/irchelp_eng.h"
 #endif
+
+static char *pIrcSyntax[][5] = {
+			// header
+			
+			{
+				"Syntax:"
+			},
+			// commands
+			
+			// others
+			// VIEWGREAT
+			{
+				"!viewgreet <#Channel>"
+			},
+			// HELP
+			{
+				"!help <Command>"
+			},
+			// VERSION
+			{
+				"!version"
+			},
+			// HELLO
+			{	  
+				"!hello"
+			},
+			// IDENT
+			{
+				"!ident [Login] <Password>"
+			},
+		
+			// Logged user
+			// LOGOFF
+			{
+				"!logoff"
+			},
+			// PASS
+			{
+				"!pass <Password>"
+			},
+			
+			// owner commands
+			// CHANMODE
+			{
+				"!chanmod <#Channel> [+|-][tnmpsklrqa] <Limit> <Keyword>"
+			},
+			// USERLIST
+			{
+				"!userlist <#Channel>"
+			},
+			// USERMODE
+			{
+				"!usermode <#Channel> [Login] [+|-][mov]",
+				"m...Master",
+				"o...Owner",
+				"v...Friend"
+			},
+			// SAY
+			{
+				"!say <#Channel> [Text]"
+			},
+			// KICK
+			{
+				"!kick <#Channel> [Nickname]"
+			},
+			// SET_TOPIC
+			{
+				"!topic <#Channel> [Topic]"
+			},
+			// SET_GREATING
+			{
+				"!greeting <#Channel> [Text]"
+			},
+			// CHANNELS
+			{
+				"!chanlist"	
+			},
+			
+			// Master commands
+			// RESTART
+			{
+				"!restart"
+			},
+			// ALLSAY
+			{
+				"!allsay [Text]"
+			},
+			// RMUSER
+			{
+				"!rmuser [Login]"
+			},
+			// NICK
+			{
+				"!nick [Nickname]"
+				
+			},
+			// DIE
+			{
+				"!die"
+			},
+			// RMCHANNEL
+			{
+				"!rmchannel <#Channel>"
+			},
+			// ADDCHANNEL
+			{
+				"!addchannel [#Channel]"
+			},
+			// JOIN
+			{
+				"!join [#Channel]"
+			},
+			// PART
+			{
+				"!part <#Channel>"
+			}
+		};
 #endif
