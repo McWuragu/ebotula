f382b134cd9b7f5a198f51b26daf117a62d09176
Thomas Stein <mail@domain.de>
2003-03-19 13:22:02 +0000

    *** empty log message ***

--- src/lang/irchelp_eng.h
+++ src/lang/irchelp_eng.h
@@ -23,7 +23,7 @@ static char *pIrcHelp[][15] = {
     // others
     // VIEWGREAT
     {
-        "This give out the greeting line of a channel",
+        "This shows the greeting line of a channel",
     },
     // HELP
     {
@@ -36,72 +36,102 @@ static char *pIrcHelp[][15] = {
     // HELLO
     {     
         "create a new account.",
+	"Every user who wants to use the bot needs an account.",
+	"Using this command you get one. Your login is the nickname,",
+	"you had when sending this command. REMEMBER to set a",
+	"password with !passwd",
     },
     // IDENT
     {
         "identify yourself to the bot",
+	"Any user who has got an account, must identify himself,",
+	"before bot commands are accepted. This is done by this command.",
     },
 
     // Logged user
     // LOGOFF
     {
-        "logoff at the bot",
+        "logoff from the bot",
+	"If you want to disconnect from the bot,",
+	"you should logoff first.",
+	
     },
     // PASS
     {
         "change the password for the current account",
+	"Selfexplaining. Change your account password (see also !help hello)",
+	"or set it if you don't have one yet.",
     },
     
     // owner commands
     // CHANMODE
     {
         "Set and remove the modes for a channel.",
+	"Change channel modes (e.g moderated [+m], topic protected +[t] etc.)",
     },
     // USERLIST
     {
         "print a list of user from the channel or all",
+	"If a channel is given as parameter, all users with an account in this",
+	"channel are listed. If no channel given, all users with an account in",
+	"any channel are listed.",
     },
     // USERMODE
     {
         "This is to set  the mod for a user",
+	"User modes are for example Channel-Operator (+o) or Voice (+v)",
     },
     // SAY
     {
-        "This give out a message of a channel",
+        "This posts a message into a channel",
+	"Needs a channel as paramater. The bot posts the given",
+	"message into this channel.",
     },
     // KICK
     {
-        "This kick a user out the channel",
+        "This kicks a user out the channel",
+	"Disconnects a user from the given channel",
     },
     // SET_TOPIC
     {
         "Set or remove the  topic of a channel",
+	"Topic is the line that can be seen near the channel name",
+	"in the channel list.",
+	"EXAMPLE: #LUGse -== LUGse - Linux User Group Senftenberg ==-",
+	"Used to discripe a channel.",
     },
     // SET_GREATING
     {
         "Set or remove the greeting line for a channel",
+	"The greeting is a message the bot sends to the channel",
+	"when a user joins the channel.",
     },
     // CHANNELS
     {
         "Print out a list of all channels from the bot",
+	"Only channels the bot is within are listed.",
     },
     
     // Master commands
     // RESTART
     {
-        "This command stop and  restart the bot.",
+        "This command stop and restart the bot.",
+	"Usefull if configuration is changed. By restarting config is read again.",
     },
     // ALLSAY
     {
         "Send a message to all channels",
+	"Similar to SAY, but message is shown in all channels the bot is within.",
     },
     // RMUSER
     {
         "This remove a user account of the bot",
+	"Selfexplaining. Accounts no longer needed can be removed this way.",
     },
     // NICK
     {
         "change the nickname of the bot",
+	"The given nickname is only valid untill the bot is restarted.",
     },
     // DIE
     {
@@ -117,7 +147,7 @@ static char *pIrcHelp[][15] = {
     },
     // JOIN
     {
-        "let the bot  join a channel",
+        "let the bot join a channel",
     },
     // PART
     {
