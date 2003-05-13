4d2a91450e322f65ef5fa46c0505793b957f8989
Uwe Strempel <mail@adresse.de>
2003-05-13 16:23:54 +0000

    - added new function to handle destroy for CallbackItem_t * - changed nichname to nickname in CallbackItem_t - changed CallbackFtk to CallbackFkt in CallbackItem_t uwe strempel

--- src/callback.h
+++ src/callback.h
@@ -2,12 +2,22 @@
 #define _CALLBACK_H
 
 typedef struct CallbackStruct {
-	char *nichname;
-	void *CallbackFtk;
+	char *nickname;
+	void *CallbackFkt;
 	unsigned int ttl;
 } CallbackItem_t;
 
-
+/* destroxCallbackItem
+ *
+ * Description:	Destroys Dataelement und sub-item of it
+ * 
+ * Parameter: 	CallbackItem_t
+ * 		-Pointer to CallbackItem_t
+ *
+ * Return:	void
+ *
+ */
+void destroyCallbackItem(CallbackItem_t *data);
 
 void ModeResetCb(char *pLine);
 void SetBanCb(char *pLine);
