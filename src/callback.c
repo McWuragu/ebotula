90dc8900721518d0c0c5c3878d50d9c6b8ed2031
Uwe Strempel <mail@adresse.de>
2003-05-13 16:22:23 +0000

    - added new function to handle destroy for CallbackItem_t * uwe strempel

--- src/callback.c
+++ src/callback.c
@@ -6,6 +6,24 @@
 
 #include"callback.h"
 
+/* destroxCallbackItem
+* 
+* Description: Destroys Dataelement und sub-item of it
+*
+* Parameter:   CallbackItem_t
+*              -Pointer to CallbackItem_t
+*
+* Return:      void
+*
+*/
+void destroyCallbackItem(CallbackItem_t *data)
+{
+      /** destroy all sub-element of CallbackIten_t **/
+        free(data->nickname);
+        free(data);
+        return;
+}
+
 
 void ModeResetCb(char *pLine){
 	DEBUG("ResetMode");
