4d08def3100c44fc060f2949c53d3aa5d6cfe20a
Steffen Laube <laube.steffengmx.de>
2004-07-03 14:33:46 +0000

    - added the handling of the logger without syslog header file - fixed the comiplation without syslog head

--- src/utilities.h
+++ src/utilities.h
@@ -17,6 +17,16 @@
 
 #ifdef HAVE_SYSLOG_H
     #include <syslog.h>
+#else
+    // fixed the missing header file syslog.h
+    #define LOG_EMERG   0
+    #define LOG_ALERT   1
+    #define LOG_CRIT    2
+    #define LOG_ERR     3
+    #define LOG_WARNING 4
+    #define LOG_NOTICE  5
+    #define LOG_INFO    6
+    #define LOG_DEBUG   7
 #endif
 
 #include "type.h"
