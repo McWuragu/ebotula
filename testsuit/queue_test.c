1058cf49f69f4afb26399c27e2910936f988e723
Steffen Laube <steffen_laube@gmx.de>
2023-12-17 21:39:57 +0100

    added forget  queue  unit test

---
+++ testsuit/queue_test.c
@@ -0,0 +1,62 @@
+/*
+ * queue_test.c
+ *
+ *  Created on: 17.12.2023
+ *      Author: slaube
+ */
+
+#include "queue_test.h"
+
+int init_queue(void) {
+	return 0;
+}
+
+int clean_queue(void) {
+	return 0;
+}
+
+
+void test_initQueue(void) {
+	CU_ASSERT_PTR_NOT_NULL(initQueue());
+}
+
+void test_isemptyQueue(void) {
+	PQueue Queue;
+	Queue = initQueue();
+
+	CU_ASSERT_TRUE(isemptyQueue(Queue));
+}
+
+void test_isemptyQueue_not(void) {
+	PQueue Queue;
+	Queue = initQueue();
+
+	QueueData testItem;
+
+	pushQueue(Queue,testItem);
+
+	CU_ASSERT_FALSE(isemptyQueue(Queue));
+}
+
+void test_isemptyQueue_after_oush_pop(void) {
+	PQueue Queue;
+	Queue = initQueue();
+
+	QueueData testItem;
+
+	pushQueue(Queue,testItem);
+	popQueue(Queue);
+
+	CU_ASSERT_TRUE(isemptyQueue(Queue));
+}
+
+void test_isfullQueue(void) {
+	PQueue Queue;
+	Queue = initQueue();
+
+	QueueData testItem;
+
+	pushQueue(Queue,testItem);
+
+	CU_ASSERT_TRUE(isfullQueue(Queue));
+}
