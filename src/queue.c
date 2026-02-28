f9b2ff84be8ff7de7b2759c77dadd048fc21452a
Steffen Laube <129774370+McWuragu@users.noreply.github.com>
2026-02-28 15:45:58 +0100

    Harden queue implementation (null checks, safe allocation, cond-wait) and add tests + code review (#24)
    * fix: harden queue push/pop and add regression tests

--- src/queue.c
+++ src/queue.c
@@ -1,6 +1,6 @@
 /* #############################################################
  *
- *  This file is a part of ebotula. 
+ *  This file is a part of ebotula.
  *
  *  Coypright (C)2003-2023 Steffen Laube <Laube.Steffen@gmx.de>
  *
@@ -16,13 +16,13 @@
  *
  *  You should have received a copy of the GNU Library General Public
  *  License along with this library; if not, write to the Free
- *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA. 
+ *  Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- * ############################################################# 
+ * #############################################################
  */
 
-/** Queue functions		 **/
-/** This Ring list -> Fifo 	**/
+/** Queue functions        **/
+/** This Ring list -> Fifo **/
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
@@ -34,208 +34,234 @@
 /* ############################################################################# */
 PQueue initQueue()
 {
-	PQueue pqueueInit;
-	
-	/** Alocationg memory for base Structure **/
-	if (!(pqueueInit=(PQueue)malloc(sizeof(Queue))))
-	{
-		/** Error not enough memory **/
-		logger(LOG_ERR,_("Couldn't allocate memory!"));
-        return NULL;
-	}
-	
-	/** creating Sentinel pointer **/
-	pqueueInit->sentinel=pqueueInit;
-	pqueueInit->queuetypeT=(QueueType)QUEUESENTINEL;
-	/* iterator not set because Queue is empty */
-	pqueueInit->iterator=pqueueInit;	
-	/** creating rest of pointer **/
-	pqueueInit->next=pqueueInit;
-	pqueueInit->prev=pqueueInit;
-	pqueueInit->queuedataData=NULL;
-	pqueueInit->longCount=0;
-	
-	/** create and init the  mutex */
-	pqueueInit->queue_mutex=(pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
-	pthread_mutex_init(pqueueInit->queue_mutex,NULL);	
-	
-    /** create and init Threadcontrolmutex */
-	pqueueInit->StopThreadCond=(pthread_cond_t *)malloc(sizeof(pthread_cond_t));
-	pthread_cond_init(pqueueInit->StopThreadCond,NULL);
+    PQueue pqueueInit;
 
-	return	pqueueInit;
+    /** Alocationg memory for base Structure **/
+    if (!(pqueueInit=(PQueue)malloc(sizeof(Queue))))
+    {
+        /** Error not enough memory **/
+        logger(LOG_ERR,_("Couldn't allocate memory!"));
+        return NULL;
+    }
+
+    /** creating Sentinel pointer **/
+    pqueueInit->sentinel=pqueueInit;
+    pqueueInit->queuetypeT=(QueueType)QUEUESENTINEL;
+    /* iterator not set because Queue is empty */
+    pqueueInit->iterator=pqueueInit;
+    /** creating rest of pointer **/
+    pqueueInit->next=pqueueInit;
+    pqueueInit->prev=pqueueInit;
+    pqueueInit->queuedataData=NULL;
+    pqueueInit->longCount=0;
+
+    /** create and init the  mutex */
+    pqueueInit->queue_mutex=(pthread_mutex_t *)malloc(sizeof(pthread_mutex_t));
+    pthread_mutex_init(pqueueInit->queue_mutex,NULL);
+
+    /** create and init Threadcontrolmutex */
+    pqueueInit->StopThreadCond=(pthread_cond_t *)malloc(sizeof(pthread_cond_t));
+    pthread_cond_init(pqueueInit->StopThreadCond,NULL);
+
+    return pqueueInit;
 }
 
 /* ############################################################################# */
 QueueExeStatus pushQueue(PQueue pqueueIn, QueueData queuedataElement)
 {
-	/** lokal auto vars **/
-	PQueue pqueueNew;/** new Element **/
-	
-	/*int intStatus;*/
-	
-	
-    pthread_mutex_lock(pqueueIn->sentinel->queue_mutex);	
-	/** cheçk if Queue not initialized **/
-	if (!pqueueIn)
-	{
-		/** creating Queue and check for Status **/
-		;
-		if (!(pqueueIn=initQueue()))
-		{
-			pthread_mutex_unlock(pqueueIn->sentinel->queue_mutex);	
-			return QUEUE_MEMORY_ALLOC_ERROR;
-		}
-	}
-	
-	/** creating elements for Queue **/
-	if ((pqueueNew=(PQueue)malloc(sizeof(Queue)))==NULL)
-	{
-		/** Error not enough memory **/
-		logger(LOG_ERR,_("Couldn't allocate memory!"));
-        pthread_mutex_unlock(pqueueIn->sentinel->queue_mutex);	
-		return QUEUE_MEMORY_ALLOC_ERROR;
-	}
-	/** assembling Queue **/
-	pqueueNew->sentinel=pqueueIn->sentinel;
-	pqueueNew->next=pqueueIn->sentinel->next;
-	pqueueNew->queue_mutex=NULL;
-	pqueueNew->StopThreadCond=NULL;
+    /** lokal auto vars **/
+    PQueue pqueueNew;/** new Element **/
 
-	if (pqueueIn->sentinel->longCount==0)
-	{
-		pqueueIn->sentinel->prev=(PQueue)pqueueNew;
-	}
-	else
-	{
-		pqueueIn->sentinel->next->prev=(PQueue)pqueueNew;
-		
-	}
+    if (!pqueueIn || !pqueueIn->sentinel || !pqueueIn->sentinel->queue_mutex ||
+        !pqueueIn->sentinel->StopThreadCond)
+    {
+        return QUEUE_NULL_POINTER_AS_IN_PARAMETER;
+    }
 
-	pqueueNew->prev=pqueueIn->sentinel;
-	pqueueIn->sentinel->next=pqueueNew;
-	pqueueNew->queuetypeT=(QueueType)QUEUENONE;
+    if (queuedataElement.t_size > 0 && queuedataElement.data == NULL)
+    {
+        return QUEUE_NULL_POINTER_AS_IN_PARAMETER;
+    }
+
+    pthread_mutex_lock(pqueueIn->sentinel->queue_mutex);
+
+    /** creating elements for Queue **/
+    if ((pqueueNew=(PQueue)malloc(sizeof(Queue)))==NULL)
+    {
+        /** Error not enough memory **/
+        logger(LOG_ERR,_("Couldn't allocate memory!"));
+        pthread_mutex_unlock(pqueueIn->sentinel->queue_mutex);
+        return QUEUE_MEMORY_ALLOC_ERROR;
+    }
+
+    /** Adding Data **/
+    if(( pqueueNew->queuedataData=(QueueData*) malloc(sizeof(QueueData)))==NULL)
+    {
+        /** Error not enough memory **/
+        logger(LOG_ERR,_("Couldn't allocate memory!"));
+        free(pqueueNew);
+        pthread_mutex_unlock(pqueueIn->sentinel->queue_mutex);
+        return QUEUE_MEMORY_ALLOC_ERROR;
+
+    }
+
+    /** allocate the memory and copy data */
+    pqueueNew->queuedataData->t_size=queuedataElement.t_size;
+    pqueueNew->queuedataData->data=NULL;
+    if (queuedataElement.t_size > 0)
+    {
+        pqueueNew->queuedataData->data=malloc(queuedataElement.t_size);
+        if (pqueueNew->queuedataData->data == NULL)
+        {
+            logger(LOG_ERR,_("Couldn't allocate memory!"));
+            free(pqueueNew->queuedataData);
+            free(pqueueNew);
+            pthread_mutex_unlock(pqueueIn->sentinel->queue_mutex);
+            return QUEUE_MEMORY_ALLOC_ERROR;
+        }
+
+        memcpy(pqueueNew->queuedataData->data,queuedataElement.data,queuedataElement.t_size);
+    }
+
+    /** assembling Queue **/
+    pqueueNew->sentinel=pqueueIn->sentinel;
+    pqueueNew->next=pqueueIn->sentinel->next;
+    pqueueNew->queue_mutex=NULL;
+    pqueueNew->StopThreadCond=NULL;
+
+    if (pqueueIn->sentinel->longCount==0)
+    {
+        pqueueIn->sentinel->prev=(PQueue)pqueueNew;
+    }
+    else
+    {
+        pqueueIn->sentinel->next->prev=(PQueue)pqueueNew;
+    }
+
+    pqueueNew->prev=pqueueIn->sentinel;
+    pqueueIn->sentinel->next=pqueueNew;
+    pqueueNew->queuetypeT=(QueueType)QUEUENONE;
     pqueueIn->sentinel->longCount++;
 
-	/** Adding Data **/
-	if(( pqueueNew->queuedataData=(QueueData*) malloc(sizeof(QueueData)))==NULL)
-	{
-		/** Error not enough memory **/
-		logger(LOG_ERR,_("Couldn't allocate memory!"));
-        pthread_mutex_unlock(pqueueIn->sentinel->queue_mutex);	
-		return QUEUE_MEMORY_ALLOC_ERROR;
-
-	}
-
-	/** allocate the memory and copy data */
-	pqueueNew->queuedataData->t_size=queuedataElement.t_size;
-	pqueueNew->queuedataData->data=malloc(queuedataElement.t_size);	
-	
-    memcpy(pqueueNew->queuedataData->data,queuedataElement.data,queuedataElement.t_size);
-    
     /* increament  the counter for the reading  threads*/
     pthread_cond_signal(pqueueIn->sentinel->StopThreadCond);
 
-    pthread_mutex_unlock(pqueueIn->sentinel->queue_mutex);	
-	return QUEUE_SUCCESS;
+    pthread_mutex_unlock(pqueueIn->sentinel->queue_mutex);
+    return QUEUE_SUCCESS;
 }
 /* ############################################################################# */
 QueueData * popQueue(PQueue pqueueIn)
 {
-	PQueue pqueueWork;
-	PQueue pqueueOld;
-	QueueData *queuedataElement;
-	pqueueWork=pqueueIn;
-	
-    
+    PQueue pqueueWork;
+    PQueue pqueueOld;
+    QueueData *queuedataElement;
+
+    if (!pqueueIn || !pqueueIn->sentinel || !pqueueIn->sentinel->queue_mutex ||
+        !pqueueIn->sentinel->StopThreadCond)
+    {
+        return (QueueData *)NULL;
+    }
+
+    pqueueWork=pqueueIn;
+
     /* protect the queue access */
-	pthread_mutex_lock(pqueueIn->sentinel->queue_mutex);	
-	
+    pthread_mutex_lock(pqueueIn->sentinel->queue_mutex);
+
     /* condition handling */
-    if (pqueueIn->sentinel->longCount<=0) {
+    while (pqueueIn->sentinel->longCount<=0) {
         pthread_cond_wait(pqueueIn->sentinel->StopThreadCond,pqueueIn->sentinel->queue_mutex);
     }
 
     /* check for valid Queue */
-	if (pqueueWork)
-	{
-		pqueueWork=(PQueue )pqueueWork->sentinel;
-		if (pqueueWork->sentinel->prev!=(PQueue)pqueueWork->sentinel)
-		{
-			/** poping **/
-			pqueueOld=(PQueue)pqueueWork->prev;
-			queuedataElement=(QueueData *)pqueueOld->queuedataData;
-			pqueueWork->prev=(PQueue )pqueueOld->prev;
-			pqueueWork->prev->next=(PQueue )pqueueWork->sentinel;
-			pqueueWork->sentinel->longCount--;
-			free(pqueueOld);
+    if (pqueueWork)
+    {
+        pqueueWork=(PQueue )pqueueWork->sentinel;
+        if (pqueueWork->sentinel->prev!=(PQueue)pqueueWork->sentinel)
+        {
+            /** poping **/
+            pqueueOld=(PQueue)pqueueWork->prev;
+            queuedataElement=(QueueData *)pqueueOld->queuedataData;
+            pqueueWork->prev=(PQueue )pqueueOld->prev;
+            pqueueWork->prev->next=(PQueue )pqueueWork->sentinel;
+            pqueueWork->sentinel->longCount--;
+            free(pqueueOld);
 
-			
-            pthread_mutex_unlock(pqueueIn->sentinel->queue_mutex);	
-			return (QueueData *)queuedataElement;
-		}	
-	} else {
-		logger(LOG_DEBUG,_("Queue is empty"));
-	}
+            pthread_mutex_unlock(pqueueIn->sentinel->queue_mutex);
+            return (QueueData *)queuedataElement;
+        }
+    } else {
+        logger(LOG_DEBUG,_("Queue is empty"));
+    }
 
-	pthread_mutex_unlock(pqueueIn->sentinel->queue_mutex);	
-	return (QueueData *)NULL;
+    pthread_mutex_unlock(pqueueIn->sentinel->queue_mutex);
+    return (QueueData *)NULL;
 }
 /* ############################################################################# */
 int isemptyQueue(PQueue pqueueIn)
 {
     boolean ret=true;
 
-    pthread_mutex_lock(pqueueIn->sentinel->queue_mutex);	
-	if (pqueueIn) {	
-		ret=(pqueueIn)->sentinel->longCount?false:true;
-	}
-    pthread_mutex_unlock(pqueueIn->sentinel->queue_mutex);	
-	return ret;
+    if (!pqueueIn || !pqueueIn->sentinel || !pqueueIn->sentinel->queue_mutex)
+    {
+        return ret;
+    }
+
+    pthread_mutex_lock(pqueueIn->sentinel->queue_mutex);
+    if (pqueueIn) {
+        ret=(pqueueIn)->sentinel->longCount?false:true;
+    }
+    pthread_mutex_unlock(pqueueIn->sentinel->queue_mutex);
+    return ret;
 }
 /* ############################################################################# */
 int isfullQueue(PQueue pqueueIn)
 {
-	return !isemptyQueue(pqueueIn);
+    return !isemptyQueue(pqueueIn);
 }
 /* ############################################################################# */
 QueueExeStatus deleteQueue(PQueue pqueueIn)
 {
     QueueExeStatus err;
 
-    err=flushQueue(pqueueIn);
-	if (err) return err;
+    if (!pqueueIn || !pqueueIn->sentinel || !pqueueIn->sentinel->queue_mutex ||
+        !pqueueIn->sentinel->StopThreadCond)
+    {
+        return QUEUE_NULL_POINTER_AS_IN_PARAMETER;
+    }
 
-	
+    err=flushQueue(pqueueIn);
+    if (err) return err;
+
+
     /* destroy all mutex and  conditions of the  queue */
-	pthread_mutex_destroy(pqueueIn->sentinel->queue_mutex);
+    pthread_mutex_destroy(pqueueIn->sentinel->queue_mutex);
     pthread_cond_destroy(pqueueIn->sentinel->StopThreadCond);
-    
+
     free(pqueueIn->sentinel->queue_mutex);
     free(pqueueIn->sentinel->StopThreadCond);
 
     /* cleanup the queue */
-	free(pqueueIn);
-	return QUEUE_SUCCESS;
+    free(pqueueIn);
+    return QUEUE_SUCCESS;
 }
 /* ############################################################################# */
 QueueExeStatus flushQueue(PQueue pqueueIn) {
-	QueueData *pTmp;
+    QueueData *pTmp;
     QueueExeStatus err;
-	
-    
+
+
     /** do it while something is in the queue **/
-	if (pqueueIn!=NULL)
-	{
-		while(isfullQueue(pqueueIn))
-		{
-			/** remove one  item and destroy it */
-			if((pTmp=popQueue(pqueueIn))){
+    if (pqueueIn!=NULL)
+    {
+        while(isfullQueue(pqueueIn))
+        {
+            /** remove one  item and destroy it */
+            if((pTmp=popQueue(pqueueIn))){
                 free(pTmp->data);
-				free(pTmp);
-			}
-		}
-        
+                free(pTmp);
+            }
+        }
+
         /* wake up all sleepers */
         pthread_cond_broadcast(pqueueIn->sentinel->StopThreadCond);
         err=QUEUE_SUCCESS;
@@ -249,25 +275,25 @@ QueueExeStatus flushQueue(PQueue pqueueIn) {
 QueueData * getNextitrQueue(PQueue pqueueIn) {
     PQueue pqueueSentinel;
     QueueData* pQueueData=NULL;
-    
+
     if (!pqueueIn) {return NULL;}
-    
+
     /* protect the queue access */
-    pthread_mutex_lock(pqueueIn->sentinel->queue_mutex);	
-	
+    pthread_mutex_lock(pqueueIn->sentinel->queue_mutex);
+
     pqueueSentinel=(PQueue )pqueueIn->sentinel;
-    
+
     /* check for valid Queue */
     if (pqueueSentinel->longCount!=0) {
-	if (pqueueSentinel!=(PQueue)pqueueSentinel->iterator->next){
-		/** poping **/
-		pqueueSentinel->iterator=(PQueue)pqueueSentinel->iterator->next;
-		pQueueData=(QueueData *)pqueueSentinel->iterator->queuedataData;	
-	}	
+        if (pqueueSentinel!=(PQueue)pqueueSentinel->iterator->next){
+            /** poping **/
+            pqueueSentinel->iterator=(PQueue)pqueueSentinel->iterator->next;
+            pQueueData=(QueueData *)pqueueSentinel->iterator->queuedataData;
+        }
     } else {
-	logger(LOG_DEBUG,_("Queue is empty"));
+        logger(LOG_DEBUG,_("Queue is empty"));
     }
 
-    pthread_mutex_unlock(pqueueIn->sentinel->queue_mutex);	
+    pthread_mutex_unlock(pqueueIn->sentinel->queue_mutex);
     return pQueueData;
 }
